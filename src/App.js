cc266bce1c6c09d7dcfca3452b4723d84c8cb6fa
daee <daee@unlv.nevada.edu>
2021-08-24 14:29:11 -0700

    finiteToFinite done

--- src/App.js
+++ src/App.js
@@ -10,11 +10,27 @@ function App() {
     { name: 'bullet point 3', x: 10.5, unit: 'pH' }
   ]);
 
-  const [ranges, setRanges] = useState([
-    { name: 'acidic', x: 6.5 },
-    { name: 'neutral', x: 8.5 },
-    { name: 'basic', x: 14.0 },
-  ]);
+  const [ranges, setRanges] = useState({
+    type: 'finiteToFinite',
+    ranges: [
+      { x: 0.0 },
+      { name: 'acidic', x: 6.5 },
+      { name: 'neutral', x: 8.5 },
+      { name: 'basic', x: 14.0 },
+    ]
+  });
+
+  /* 
+  'finiteToFinite': {
+    type: 'finiteToFinite',
+    ranges: [
+      { x: 1.0 }, //our start point
+      { name: 'acidic', x: 6.5 },
+      { name: 'neutral', x: 8.5 },
+      { name: 'basic', x: 14.0 }, //14.0 would be end point
+    ]
+  }
+  */
 
   const randomizePoints = () => {
     let numPoints = Math.floor(Math.random() * 6);
@@ -23,7 +39,7 @@ function App() {
     for (let i = 0; i < numPoints; i++) {
       points.push({
         name: `bulet point ${i + 1}`,
-        x: (Math.random() * 14.0).toPrecision(2),
+        x: (Math.random() * 13.0 + 1).toPrecision(2),
         unit: 'pH'
       });
     }
@@ -33,6 +49,7 @@ function App() {
 
   return (
     <div className="App">
+      {/* the style on the div is just for a test on screen responsiveness */}
       <div style={{ width: '80%', backgroundColor: 'gray' }}>
         <BulletGraph points={points} sensorRanges={ranges} />
       </div>
--- src/BulletGraph.js
+++ src/BulletGraph.js
@@ -3,7 +3,22 @@ import React, { useEffect, useRef } from 'react';
 const BulletGraph = (props) => {
     const canvasRef = useRef(null);
 
+    //'consts' set in useeffect since it is dependent on our size
+    let GRAPH_HEIGHT = 0;
+    let GRAPH_Y = 0;
+    let TEXT_Y = 0;
+
     let { points, sensorRanges, colors } = props;
+    const { ranges, type } = sensorRanges;
+
+    /*
+    ranges can be of different types
+        zeroToInfinite = [{name: string, x: number}] //if last element, x should be nothing
+        zeroToFinite = [{name: string, x: number}] //if last element, x should have value
+        finiteToFinite =[{name: string, x: number}] //first element should just be {x: number}, no name
+        infiniteToInfinite = null //no ranges here, labels will just be 'negative' and 'positive'
+        percentage = [{name: string, x: float}] //x should be 0.0 through 1.0, but total sum should equal 1.0
+    */
 
     //temp data for now
     //NOTE: range input data not enough for ranges, we need a start, not just the end
@@ -12,13 +27,87 @@ const BulletGraph = (props) => {
     colors = [
         '#2a9d8f',
         '#e9c46a',
-        '#e76f51'
+        '#e76f51',
+        '#2a9d8f',
+        '#e9c46a',
+        '#e76f51',
+        '#2a9d8f',
+        '#e9c46a',
+        '#e76f51',
     ];
 
 
     //HELPER FUNCTIONS
-    const getWidthOfRange = (start, end, totalRange, totalWidth) => {
-        //get the percentage of total width
+    const drawRanges = (ctx) => {
+        let start = 0;
+
+        if (type === 'finiteToFinite') {
+            start = ranges[0].x;
+        }
+
+        let prevWidth = 0;
+        for (let i = 0; i < ranges.length; i++) {
+            if (i === 0 && type === 'finiteToFinite') continue;
+
+            if ((i === 1 && type === 'finiteToFinite') || i === 0) {
+                //begin
+                //get width 
+                let width = getWidthOfRange(start, ranges[i].x);
+
+                ctx.fillStyle = colors[i];
+                ctx.fillRect(0, GRAPH_Y, width, GRAPH_HEIGHT);
+
+                ctx.fillStyle = 'black';
+                ctx.textAlign = "start";
+                ctx.font = '18px serif';
+                ctx.fillText(start, 0, TEXT_Y);
+
+                drawLabel(ctx, ranges[i].name, 0, ranges[i].x);
+                prevWidth += width;
+            } else {
+                //end
+                let width = getWidthOfRange(ranges[i - 1].x, ranges[i].x);
+
+                ctx.fillStyle = colors[i];
+                ctx.fillRect(prevWidth, GRAPH_Y, width, GRAPH_HEIGHT);
+
+                ctx.fillStyle = 'black';
+                ctx.textAlign = "center";
+                ctx.font = '18px serif';
+                ctx.fillText(ranges[i - 1].x, prevWidth, TEXT_Y);
+
+                drawLabel(ctx, ranges[i].name, ranges[i - 1].x, ranges[i].x);
+
+                prevWidth += width;
+
+                //last element
+                if (i === ranges.length - 1) {
+                    ctx.fillStyle = 'black';
+                    ctx.textAlign = "end";
+                    ctx.font = '18px serif';
+                    ctx.fillText(ranges[i].x, prevWidth, TEXT_Y);
+                }
+            }
+        }
+
+    };
+
+    const getTotalRange = () => {
+        if (type === 'finiteToFinite') {
+            let start = ranges[0].x;
+            let end = ranges[ranges.length - 1].x;
+            return end - start;
+        }
+    };
+
+    const getWidthOfRange = (start, end) => {
+        let canvas = canvasRef.current;
+
+        let totalWidth = canvas.width;
+        let totalRange = getTotalRange();
+
+        console.log(totalRange);
+
         let d = end - start;
         let p = d / totalRange;
 
@@ -26,10 +115,16 @@ const BulletGraph = (props) => {
     };
 
     const getWorldX = (x) => {
-        const totalRange = sensorRanges[sensorRanges.length - 1].x;
+        const totalRange = getTotalRange();
         const totalWidth = canvasRef.current.width;
 
-        return (x / totalRange) * totalWidth;
+        let start = 0;
+
+        if (type === 'finiteToFinite') {
+            start = ranges[0].x;
+        }
+
+        return ((x - start) / totalRange) * totalWidth;
     };
 
     const drawPoint = (ctx, startx, starty) => {
@@ -49,7 +144,6 @@ const BulletGraph = (props) => {
     };
 
     const drawLabel = (ctx, label, left, right) => {
-        console.log(left, right);
         const LABEL_Y = 130;
         //get world coords
         const wleft = getWorldX(left);
@@ -64,65 +158,17 @@ const BulletGraph = (props) => {
 
     //DRAW
     const draw = ctx => {
-        let canvas = canvasRef.current;
-        //CONSTANTS (CHANGE THIS TO BE PROPS WITH DEFAULT VALUES EVENTUALLY)
-        const GRAPH_HEIGHT = 20;
-        const GRAPH_Y = canvas.height / 2 - GRAPH_HEIGHT / 2;
-        const TEXT_Y = GRAPH_Y + GRAPH_HEIGHT + 20; //20 is padding
-
-        //DRAW THE RANGES
-        let totalWidth = canvas.width;
-
-        //we are going to have to change the way we get ranges once we have standard data
-        let totalRange = sensorRanges[sensorRanges.length - 1].x;
-
-        let prevWidth = 0;
-        for (let i = 0; i < sensorRanges.length; i++) {
-            if (i === 0) {
-                //begin
-                //get width 
-                let width = getWidthOfRange(0, sensorRanges[i].x, totalRange, totalWidth);
-
-                ctx.fillStyle = colors[i];
-                ctx.fillRect(0, GRAPH_Y, width, GRAPH_HEIGHT);
-
-                ctx.fillStyle = 'black';
-                ctx.textAlign = "start";
-                ctx.font = '18px serif';
-                ctx.fillText('0.0', 0, TEXT_Y);
-
-                drawLabel(ctx, sensorRanges[i].name, 0, sensorRanges[i].x);
-                prevWidth += width;
-            } else {
-                //end
-                let width = getWidthOfRange(sensorRanges[i - 1].x, sensorRanges[i].x, totalRange, totalWidth);
-
-                ctx.fillStyle = colors[i];
-                ctx.fillRect(prevWidth, GRAPH_Y, width, GRAPH_HEIGHT);
-
-                ctx.fillStyle = 'black';
-                ctx.textAlign = "center";
-                ctx.font = '18px serif';
-                ctx.fillText(sensorRanges[i - 1].x, prevWidth, TEXT_Y);
-
-                drawLabel(ctx, sensorRanges[i].name, sensorRanges[i - 1].x, sensorRanges[i].x);
-
-                prevWidth += width;
-
-                //last element
-                if (i === sensorRanges.length - 1) {
-                    ctx.fillStyle = 'black';
-                    ctx.textAlign = "end";
-                    ctx.font = '18px serif';
-                    ctx.fillText(sensorRanges[i].x, prevWidth, TEXT_Y);
-                }
-            }
+        //DRAW THE RANGES or RANGE
+        if (ranges) {
+            drawRanges(ctx);
+        } else {
+            //just draw default range
+            drawRanges(ctx);
         }
 
         //DRAW THE POINTS
         for (let i = 0; i < points.length; i++) {
             drawPoint(ctx, getWorldX(points[i].x), GRAPH_HEIGHT + GRAPH_Y);
-
             //draw labels eventually
         }
 
@@ -136,6 +182,13 @@ const BulletGraph = (props) => {
         canvas.style.height = "100%";
         canvas.width = canvas.offsetWidth;
         canvas.height = canvas.offsetHeight;
+
+        //set our 'consts'
+        //ignore warning here, this is set on each render anyway
+        GRAPH_HEIGHT = 20;
+        GRAPH_Y = canvas.height / 2 - GRAPH_HEIGHT / 2;
+        TEXT_Y = GRAPH_Y + GRAPH_HEIGHT + 20; //20 is padding
+
         const context = canvas.getContext('2d');
         const updateSize = (e) => {
             canvas.style.width = "100%";
@@ -149,8 +202,6 @@ const BulletGraph = (props) => {
             draw(context);
         };
         window.addEventListener('resize', updateSize);
-
-
 
         //clear the canvas on redraw
         context.clearRect(0, 0, canvas.width, canvas.height);
