cca0e737c3f5c6be22e5b269f60692481e7dadf5
daee <daee@unlv.nevada.edu>
2021-08-24 20:29:25 -0700

    rounded shapes + make points rounded

--- src/BulletGraph.js
+++ src/BulletGraph.js
@@ -268,16 +268,68 @@ const BulletGraph = (props) => {
         ctx.fillStyle = "white";
         ctx.strokeStyle = "gray";
         ctx.lineWidth = 3;
-        ctx.beginPath();
-        ctx.moveTo(startx, starty);
-        ctx.lineTo(startx + GRAPH_HEIGHT * 0.75, starty - GRAPH_HEIGHT);
-        ctx.lineTo(startx - GRAPH_HEIGHT * 0.75, starty - GRAPH_HEIGHT);
-        ctx.lineTo(startx, starty);
+        const borderRadius = 5;
+        let points = [
+            [startx, starty],
+            [startx + GRAPH_HEIGHT * 0.75, starty - GRAPH_HEIGHT],
+            [startx - GRAPH_HEIGHT * 0.75, starty - GRAPH_HEIGHT]
+        ];
+        drawPolygon(ctx, points, borderRadius);
         ctx.fill();
         ctx.stroke();
 
         //reset
         ctx.lineWidth = 1;
+    };
+
+    //https://stackoverflow.com/questions/1255512/how-to-draw-a-rounded-rectangle-using-html-canvas
+    const drawPolygon = (ctx, pts, radius) => {
+        const getRoundedPoints = (pts, radius) => {
+            var i1, i2, i3, p1, p2, p3, prevPt, nextPt,
+                len = pts.length,
+                res = new Array(len);
+            for (i2 = 0; i2 < len; i2++) {
+                i1 = i2 - 1;
+                i3 = i2 + 1;
+                if (i1 < 0) {
+                    i1 = len - 1;
+                }
+                if (i3 == len) {
+                    i3 = 0;
+                }
+                p1 = pts[i1];
+                p2 = pts[i2];
+                p3 = pts[i3];
+                prevPt = getRoundedPoint(p1[0], p1[1], p2[0], p2[1], radius, false);
+                nextPt = getRoundedPoint(p2[0], p2[1], p3[0], p3[1], radius, true);
+                res[i2] = [prevPt[0], prevPt[1], p2[0], p2[1], nextPt[0], nextPt[1]];
+            }
+            return res;
+        };
+
+        const getRoundedPoint = (x1, y1, x2, y2, radius, first) => {
+            var total = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)),
+                idx = first ? radius / total : (total - radius) / total;
+            return [x1 + (idx * (x2 - x1)), y1 + (idx * (y2 - y1))];
+        };
+
+        if (radius > 0) {
+            pts = getRoundedPoints(pts, radius);
+        }
+        var i, pt, len = pts.length;
+        ctx.beginPath();
+        for (i = 0; i < len; i++) {
+            pt = pts[i];
+            if (i === 0) {
+                ctx.moveTo(pt[0], pt[1]);
+            } else {
+                ctx.lineTo(pt[0], pt[1]);
+            }
+            if (radius > 0) {
+                ctx.quadraticCurveTo(pt[2], pt[3], pt[4], pt[5]);
+            }
+        }
+        ctx.closePath();
     };
 
     const drawLabel = (ctx, label, left, right) => {
@@ -329,7 +381,6 @@ const BulletGraph = (props) => {
 
         //set our 'consts'
         //ignore warning here, this is set on each render anyway
-        GRAPH_HEIGHT = 20;
         GRAPH_Y = canvas.height / 2 - GRAPH_HEIGHT / 2;
         TEXT_Y = GRAPH_Y + GRAPH_HEIGHT + 20; //20 is padding
 
